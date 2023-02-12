0ed0156f544ac2283f2a420cdfff37ce63ca218b
Turkyeh <Turkyeh.yousef@gmail.com>
2023-02-12 15:40:17 +0300

    add a jquary parrt

---
+++ chic/public/jquery.js
@@ -0,0 +1,35 @@
+$('p').hide();
+
+
+// window.onload=function(){alert("Wellcome Turkyeh")}
+
+$(document).ready(function(){
+    console.log("thes page use a jquary liblary");
+
+    $('h1').hide();
+
+    // // This is the sourt hand code
+// $("button").click(function(){
+
+//     $('h1').hide();
+});
+
+// $function(){
+// // This is the sourt hand code
+// $("button").click(function(){
+
+//     $('h1').hide();
+// })
+// });
+
+
+$(document).ready(function(){
+  
+
+    // // This is the sourt hand code
+$(button).click(function (e) { 
+
+   ();
+    
+}
+}
